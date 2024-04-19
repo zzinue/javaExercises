3821592b39db8fdbddb35fabaebcacdb6b25b808
zzinue <zzinue@gmail.com>
2024-04-18 20:00:07 -0600

    segundo commit con nueva clase decisiones

---
+++ src/decisiones.java
@@ -0,0 +1,14 @@
+public class decisiones {
+    public static void main(String[] args) {
+
+        int fechaDeLanzamiento=1999;
+        boolean incluidoEnElPlan=true;
+        double notaDeLaPelicula=8.2;
+
+        if(fechaDeLanzamiento>2022){
+            System.out.println("Peliculas mas populares");
+        }else{
+            System.out.println("Esta pelicula no es reciente");
+        }
+    }
+}
