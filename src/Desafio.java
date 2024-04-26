923d2743f3b77f8f477c88b2a1a69d6c20bb04dc
zzinue <zzinue@gmail.com>
2024-04-25 19:40:14 -0600

    ejemplos con break de saldo

--- src/Desafio.java
+++ src/Desafio.java
@@ -1,12 +1,57 @@
+import java.util.Scanner;
+
 public class Desafio {
     public static void main(String[] args) {
         String nombre="Tony stark";
         String tipoDeCuenta="Corriente";
         double saldo=1599.99;
+        int opcion=0;
 
-        System.out.println("************");
+        System.out.println("**********************************");
         System.out.println("\n Nommbre del cliente: "+ nombre);
         System.out.println("El tipo de cuenta es:"+tipoDeCuenta);
         System.out.println("Su saldo disponible es:"+ saldo);
+        System.out.println("\n********************************");
+
+        String menu = """
+                *** Escriba el numero de la opcion deseadad***
+                1.- Consultar saldo
+                2.- Retirar
+                3.- Depositar
+                9.- Salir
+                """;
+
+        Scanner teclado=new Scanner(System.in);
+        while(opcion !=9){
+            System.out.println(menu);
+            opcion=teclado.nextInt();
+
+            switch (opcion){
+                case 1:
+                    System.out.println("El saldo actuazliado es:"+ saldo +"$");
+                    break;
+                case 2:
+                    System.out.println("Cual es el valor que vas a retirar?");
+                    double valorARetirar=teclado.nextDouble();
+                    if(saldo> valorARetirar){
+                        System.out.println("Saldo insuficiente");
+                    } else{
+                        saldo= saldo-valorARetirar;
+                        System.out.println("El saldo actualizado es:"+saldo);
+                    }
+                    break;
+                case 3:
+                    System.out.println("Cual es el valor que desea depositar");
+                    double valorADepositar=teclado.nextDouble();
+                    saldo=saldo+valorADepositar;
+                    System.out.println("Tu saldo actualizado es:"+ saldo);
+                    break;
+                case 9:
+                    System.out.println("saliendo del programa, gracias por utilizar nuestros servicios");
+                    break;
+                default:
+                    System.out.println("opcion no valida");
+            }
+        }
     }
 }
