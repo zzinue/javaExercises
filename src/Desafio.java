c503919cc80d8d1968857bbc7ff9a0f0de2021a3
zzinue <zzinue@gmail.com>
2024-04-25 18:49:36 -0600

    desafio y ejemplos alura

---
+++ src/Desafio.java
@@ -0,0 +1,12 @@
+public class Desafio {
+    public static void main(String[] args) {
+        String nombre="Tony stark";
+        String tipoDeCuenta="Corriente";
+        double saldo=1599.99;
+
+        System.out.println("************");
+        System.out.println("\n Nommbre del cliente: "+ nombre);
+        System.out.println("El tipo de cuenta es:"+tipoDeCuenta);
+        System.out.println("Su saldo disponible es:"+ saldo);
+    }
+}
